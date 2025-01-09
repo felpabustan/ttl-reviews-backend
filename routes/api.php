50317764c52057eec9fa75d7808ddf591076be5a
Fel <fel@mapletreemedia.com>
2025-01-09 13:44:30 +0800

    add version prefix for api

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Review\ReviewController;
 use App\Http\Middleware\EnsureTokenIsValid;
 
-Route::prefix('reviews')->group(function () {
-  Route::get('/', [ReviewController::class, 'index']);
-  Route::post('/', [ReviewController::class, 'store']);
-  Route::get('/{id}', [ReviewController::class, 'show']);
-  Route::put('/{id}', [ReviewController::class, 'update']);
-  Route::delete('/{id}', [ReviewController::class, 'destroy']);
-  Route::post('/import', [ReviewController::class, 'import']);
-})->middleware(EnsureTokenIsValid::class);;
+Route::prefix('v1')->group(function () {
+  Route::prefix('reviews')->group(function () {
+      Route::get('/', [ReviewController::class, 'index']);
+      Route::post('/', [ReviewController::class, 'store']);
+      Route::get('/{id}', [ReviewController::class, 'show']);
+      Route::put('/{id}', [ReviewController::class, 'update']);
+      Route::delete('/{id}', [ReviewController::class, 'destroy']);
+      Route::post('/import', [ReviewController::class, 'import']);
+  })->middleware(EnsureTokenIsValid::class);
+});
